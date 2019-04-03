467cfbb4ea8a6e8d2b3cb5f792694a247b141b23
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-04-03 18:26:20 -0300

    Iniciada função de captura de senha

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* recebeSenha() {
+	int i = 0, j;
+	char* a;
+	char c;
+
+	a = malloc(sizeof(char));
+	c = getchar();
+	/* 10 representa a tecla "ENTER"*/
+	while (c != -1 && c != 10) {
+		a[i] = c;
+		c = getchar();
+		i++;
+	}
+	i = strlen(a);
+	j = i;
+	while (i < 16) {
+		a[i] = a[i - j];
+		i++;
+	}
+	return a;
+}
+
+int geraChaveK() {
+	return 0;
+}
 
 int main() {
+	char* a;
 	printf("Falta Fazer o EP :D\n");
+	a = recebeSenha();
+	printf("%s\n", a);
 	return 0;
 }
