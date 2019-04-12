5701b8063a508fe2ed6dac87d2937c9908315bd1
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-04-12 15:28:31 -0300

    Adicionado solução do exercicio 2 da lista 2

--- Lista 2/millerRabin.py
+++ Lista 2/millerRabin.py
@@ -1,34 +1,26 @@
 def millerRabin():
 	n = int(input("Entre com n: "))
 	a = int(input("Entre com a: "))
-	s = 0
+	t = 0
 	if (n%2 == 0):
 		print("Numero", n, "é composto")
 		return
 	else:
-		d = n - 1
-		while (d%2 == 0):
-			d = int(d/2)
-			s = s + 1
-	print("Imprimindo valores iniciais a e d:")		
-	print("a =", a, "d =", d)		
-	base = a**d
-	print("base = a**d =", a, "^", d)
+		c = n - 1
+		while (c%2 == 0):
+			c = int(c/2)
+			t = t + 1
+	print("Imprimindo valores iniciais t e c:")		
+	print("t =", t, "c =", c)		
+	base = a**c
+	print("base = a**c =", a, "^", c)
 	r = 0
-	print("Entrando no loop r =", r, "s =", s)
-	while (r < s):
+	print("Entrando no loop r =", r, "t =", t)
+	while (r <= t):
 		mod = (base**(2**r))%n
-		print("r =", r, "s =", s, "mod =", mod)
-		if(mod == 1):
-			print("Número", n, "é composto")
-			break
-		elif(mod == n-1):
-			print("Número", n, "é provavelmente primo")
-			return		
+		print("r =", r, "t =", t, "mod =", mod)
 		r = r + 1
-	print("a =", a, "é testemunha contra a primalidade de n.")	
-	return
-	
+		
 def main():
 	millerRabin()
 	return
