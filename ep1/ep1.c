a7f5498105846f7928ec76471498d50108bb26e3
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-04-15 18:10:33 -0300

    Efetuado o recebimento de senha em characteres. Adicionado LEIAME

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -1,37 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
+
+bool confereSenha(char *senha, int n) {
+	int numChar = 0, num09 = 0, i;
+
+	/* Conferindo o tamanho minimo desejado
+	*/
+	if (n < 8) return false;
+
+	/* Conferindo se temos pelo menos 2 letras e 2 
+	// algarismos decimais (poderia ser mais eficiente
+	// porem julguei esta forma mais legivel).
+	*/
+	for (i = 0; i < n; i++) {
+		if (isalpha(senha[i])) numChar++;
+		else if (isdigit(senha[i])) num09++;
+	}
+	if (numChar < 2) return false;
+	if (num09 < 2) return false;
+	return true;
+}
 char* recebeSenha() {
 	int i = 0, j;
-	char* a;
+	char* senha;
 	char c;
 
-	a = malloc(sizeof(char));
+	senha = malloc(16 * sizeof(char));
 	c = getchar();
-	/* 10 representa a tecla "ENTER"*/
+	/* 10 representa a tecla "ENTER"
+	// -1 representa o fim de um arquivo
+	*/
 	while (c != -1 && c != 10) {
-		a[i] = c;
+		senha[i] = c;
 		c = getchar();
 		i++;
 	}
-	i = strlen(a);
+
+	/*conferindo se a senha esta OK
+	*/
+	if (!confereSenha(senha, strlen(senha))) {
+		return NULL;
+	}
+	/*Tudo OK, completamos a senha se necessario
+	*/
+	i = strlen(senha);
 	j = i;
+
+	/*completa senha caso necessário
+	*/
 	while (i < 16) {
-		a[i] = a[i - j];
+		senha[i] = senha[i - j];
 		i++;
 	}
-	return a;
+
+	return senha;
 }
 
-int geraChaveK() {
+int geraChaveK(char *senha) {
+	int i;
+	for (i = 0; i < 16; i++) {
+		printf("%X ", senha[i]);
+	}
 	return 0;
 }
 
 int main() {
-	char* a;
+	char* senha = NULL;
 	printf("Falta Fazer o EP :D\n");
-	a = recebeSenha();
-	printf("%s\n", a);
+	while (senha == NULL) {
+		printf("Entre com uma senha\n");
+		senha = recebeSenha();
+	}
+	geraChaveK(senha);
 	return 0;
 }
