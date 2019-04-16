931672a3d9d13c685a8e56ca467e924d2301c8e0
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-04-16 12:51:17 -0300

    Adicionado teste de somaBinario64

--- ep1/teste.c
+++ ep1/teste.c
@@ -1,17 +1,19 @@
 #include "funcoesEp1.h"
 
 int main() {
-    int *bin;
+    int *bin, *a, *b, *c;
     int *assert;
     int i;
 
+    /*Conferindo hexaParaBinario
+    */
     bin = malloc(8 * sizeof(int));
     assert = malloc(8 * sizeof(int));
     assert[0] = 1;
     assert[1] = 0;
     assert[2] = 0;
     assert[3] = 0;
-    assert[4] = 1;
+    assert[4] = 0;
     assert[5] = 0;
     assert[6] = 0;
     assert[7] = 0;
@@ -23,5 +25,48 @@ int main() {
         }
     }
 
+
+
+    /*Conferindo somaBinario64
+    */
+    a = malloc(8 * sizeof(int));
+    b = malloc(8 * sizeof(int));
+    c = malloc(8 * sizeof(int));    
+    a[0] = 0;
+    a[1] = 0;
+    a[2] = 0;
+    a[3] = 0;
+    a[4] = 0;
+    a[5] = 0;
+    a[6] = 0;
+    a[7] = 1;
+    b[0] = 1;
+    b[1] = 0;
+    b[2] = 0;
+    b[3] = 0;
+    b[4] = 0;
+    b[5] = 0;
+    b[6] = 0;
+    b[7] = 1;
+    assert[0] = 1;
+    assert[1] = 0;
+    assert[2] = 0;
+    assert[3] = 0;
+    assert[4] = 0;
+    assert[5] = 0;
+    assert[6] = 0;
+    assert[7] = 0;    
+    c = somaBinario64(a, b);
+    for(i = 0; i < 8; i++) {
+        if (c[i] - assert[i] != 0) {
+            printf("Erro em somaBinario64!\n");
+        }
+    }
+
+    free(bin);
+    free(a);
+    free(b);
+    free(c);
+    free(assert);
     return 0; 
 }
