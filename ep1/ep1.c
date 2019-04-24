ed56acb5d971ab7652fc63cc7038fc1b7a3a27dc
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-04-24 10:33:43 -0300

    Adicionado abertura de arquivos na encriptação

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -1,5 +1,7 @@
 #include "funcoesEp1.h"
 
+/* Funcao que explica o uso do programa
+*/
 void instrucoesDeUso() {
 	printf("Modo (1) Para criptografar arquivos:\n\
 programa -c -i <arquivo de entrada> -o\
@@ -14,7 +16,10 @@ programa -2 -i <arquivo de entrada> -p <senha>\n\n");
 	return;
 }
 
-bool confereChamadaCripto(int argc, char *argv) {
+/* Funcao que confere se o uso do programa para criptografar
+// o arquivo está correto
+*/
+bool confereChamadaCripto(int argc, char **argv) {
 	if (argc < 8) return false;
 	if (strcmp(argv[2], "-i") != 0) return false;
 	if (strcmp(argv[4], "-o") != 0) return false;
@@ -22,20 +27,31 @@ bool confereChamadaCripto(int argc, char *argv) {
 	return true;
 }
 
-void criptografar(int argc, char *argv)  {
+/* Funcao que criptografa o arquivo de entrada
+*/
+void criptografar(int argc, char **argv)  {
+	FILE *entrada, *saida;
+
 	if (!confereChamadaCripto(argc, argv)) { 
 		printf("Para criptografar arquivos:\n\
 programa -c -i <arquivo de entrada> -o\
 <arquivo de saída> -p <senha> -a\n\n");
 		return;
 	}
+	entrada = fopen(argv[3], "r");
+	if (!entrada) {
+		printf("Problemas ao abrir arquivo a ser criptografado!\n");
+		return;
+	}
+	saida = fopen(argv[5], "w");
+
 	return;
 }
 
+
+/* MAIN
+*/
 int main(int argc, char *argv[]) {
-	char* senha = NULL;
-	int* chaveK = NULL;
-	int i;
 	
 	if (argc < 6) {
 		instrucoesDeUso();
@@ -43,7 +59,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	if (strcmp(argv[1], "-c") == 0) {
-		criptografar();
+		criptografar(argc, argv);
 	}
 
 	else if (strcmp(argv[1], "-d") == 0) {
