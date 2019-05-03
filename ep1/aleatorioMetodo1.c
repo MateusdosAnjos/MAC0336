313b14cee9ff539d5dec6427009b6f7cc1d83dc2
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-05-02 22:29:57 -0300

    Criptografado todos os blocos necessarios
    Signed-off-by: MateusdosAnjos <mateus.a.anjos@gmail.com>

--- ep1/aleatorioMetodo1.c
+++ ep1/aleatorioMetodo1.c
@@ -34,12 +34,27 @@ int **realocaMatriz(int **a, int n) {
 	return realoc;	
 }
 
+/* Funcao que calcula a distancia de Hamming entre 2 blocos
+// de 128 bits
+*/
+int hamming(int *a, int *b) {
+	int i, dist;
+
+	dist = 0;
+
+	for (i = 0; i < 128; i++) {
+		if (a[i] != b[i]) dist++;
+	}
+
+	return dist;
+}
+
 /* Funcao que calcula o valor de aleatoriedade (entropia)
 // pelo metodo 1 descrito no enunciado
 */
 void aleatorioMetodo1(int argc, char **argv) {
 	FILE *entrada;
-	int *chaveK = NULL, *bin = NULL;
+	int *chaveK = NULL, *bin = NULL, *X = NULL, *Y = NULL;
 	int **subChavesK = NULL, **vetEntra = NULL, **vetAlter = NULL,
 	**vetEntraC = NULL, **vetAlterC = NULL;
 	int i, j, k, numBlocos;
@@ -86,7 +101,7 @@ com pelo menos 2 letras e 2 algarismos decimais!\n");
 	/******************************************************/		
 	numBlocos = 0;
 	k = 2;
-	vetEntra = malloc(k * sizeof(int *));
+	vetEntra = calloc(k, sizeof(int *));
 	for (i = 0; i < k; i++) {
 		vetEntra[i] = calloc(128, sizeof(int));
 	}
@@ -114,14 +129,58 @@ com pelo menos 2 letras e 2 algarismos decimais!\n");
 	/*****************************************************/
 	/* Copia para vetAlter os valores da entrada         */
 	/*****************************************************/		
-	vetAlter = malloc(numBlocos * sizeof(int *));
+	vetAlter = calloc(numBlocos, sizeof(int *));
 	for (i = 0; i < numBlocos; i++) {
-		vetAlter[i] = malloc(128 * sizeof(int));
+		vetAlter[i] = calloc(128, sizeof(int));
 		for (j = 0; j < 128; j++) {
 			vetAlter[i][j] = vetEntra[i][j];
 		}
 	}
+	/*****************************************************/
+	/* Aloca espaço para os blocos criptografados        */
+	/*****************************************************/		
+	vetEntraC = calloc(numBlocos, sizeof(int *));
+	vetAlterC = calloc(numBlocos, sizeof(int *));
+	/**************************************************/
+	/* Inicializa Y(V1) para o xor inicial            */
+	/**************************************************/	
+	Y = malloc(128 * sizeof(int));
+	for (i = 0; i < 128; i++) {
+		Y[i] = 1;
+	}
+	/*****************************************************/
+	/* Criptografa vetEntra e armazena em vetEntraC      */
+	/*****************************************************/
+	for (i = 0; i < numBlocos; i++) {		
+		X = xor(vetEntra[i], Y, 128);	
+		vetEntraC[i] = K128(X, subChavesK, 12);
+		Y = vetEntraC[i];
+	}			
+	/*****************************************************/
+	/* Modifica bit a bit de vetAlter                    */
+	/*****************************************************/	
+	for (i = 0; i < numBlocos; i++) {
+		for (j = 0; j < 128; j++) {
+				for (k = 0; k < 128; k++) {
+					Y[k] = 1;
+				}
+			/* Bit modificado */
+			vetAlter[i][j] = (vetAlter[i][j] + 1) % 2;
+			/*****************************************************/
+			/* Criptografa vetAlter e armazena em vetAlterC      */
+			/*****************************************************/
+			for (k = 0; k < numBlocos; k++) {		
+				X = xor(vetAlter[i], Y, 128);	
+				vetAlterC[i] = K128(X, subChavesK, 12);
+				Y = vetAlterC[i];
+			}				
 
 
+
+
+
+			vetAlter[i][j] = (vetAlter[i][j] + 1) % 2;
+		}
+	}
 	return ;
 }
